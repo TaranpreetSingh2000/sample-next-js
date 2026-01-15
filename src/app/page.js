9d7fbc047f19640ae24d001ab4997835680e45fb
TaranpreetSingh2000 <88654743+TaranpreetSingh2000@users.noreply.github.com>
2026-01-15 12:41:09 +0530

    add the code

--- src/app/page.js
+++ src/app/page.js
@@ -56,7 +56,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Documentation
+            Documentation on time appears as it aopears
           </a>
         </div>
       </main>
